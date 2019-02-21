d650c6aa25f82cd95060fcbc9eb2d2a642605194
plee19 <plee19@my.wctc.edu>
2019-02-20 19:24:16 -0600

    Week 2/3 pushed

---
+++ First/src/us/plee19/first/FirstServlet.java
@@ -0,0 +1,26 @@
+package us.aaraujo1.first;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FirstServlet extends javax.servlet.http.HttpServlet {
+    /*protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }*/
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String html = "<html><body>Hi. I received param1=" + request.getParameter("param1") +
+                " via POST</body><html>";
+        response.getOutputStream().println(html);
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String html = "<html><body>Hi. I received param1=" + request.getParameter("param1") +
+                " via GET</body><html>";
+        response.getOutputStream().println(html);
+    }
+}
