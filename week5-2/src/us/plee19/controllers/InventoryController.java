c38d717b0f5efe533e8aff9f834b331d0cafd4a0
plee19 <plee19@my.wctc.edu>
2019-04-01 09:42:08 -0500

    WIP on 5B-B

--- week5-2/src/us/plee19/controllers/InventoryController.java
+++ week5-2/src/us/plee19/controllers/InventoryController.java
@@ -11,32 +11,53 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URLEncoder;
+import java.sql.*;
 
 @WebServlet(name = "InventoryController")
 public class InventoryController extends HttpServlet {
     protected void doPost(HttpServletResponse response) throws ServletException, IOException {
-
         PrintWriter out = response.getWriter();
+        final String DB_URL = "jdbc:derby:CatalogDB";
+        Statement stmt = null;
+        Connection conn = null;
+        try {
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL);
+            System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT ProdNum, Description, Price FROM Catalog";
+            ResultSet rs = stmt.executeQuery(sql);
 
-        out.println("<html><body><table><tr><th>Product ID</th><th>Product Name</th><th>Price</th></tr>");
+            while (rs.next()) {
+                String id = rs.getString("ProdNum");
+                double price = rs.getDouble("Price");
+                String description = rs.getString("Description");
 
-
-        InventoryService inventory = new InventoryService();
-        for (Product p : inventory.getAllProducts()
-        ) {
-            out.println("<tr><td>" + p.getId() + "</td><td>" + p.getName() + "</td><td>" + p.getPrice() + "</td></tr>");
+                out.print("ID: " + id.trim());
+                out.print(", Price: " + price);
+                out.println(", Description: " + description);
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null) stmt.close();
+            } catch (SQLException se2) {
+            }
+            try {
+                if (conn != null) conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
         }
 
-        out.println("</body></html>");
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-        PrintWriter out = response.getWriter();
-
-        out.println("<html><body><table><tr><th>Product ID</th><th>Product Name</th><th>Price</th></tr>");
-
+        /*out.println("<html><body><table><tr><th>Product ID</th><th>Product Name</th><th>Price</th></tr>");
 
 
         InventoryService inventory = new InventoryService();
@@ -46,6 +67,63 @@ public class InventoryController extends HttpServlet {
             out.print("href=\"DeleteController?product=" + URLEncoder.encode(p.getName(),"UTF-8") + "\">Remove");
         }
 
-        out.println("</body></html>");
+        out.println("</body></html>");*/
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        PrintWriter out = response.getWriter();
+        final String DB_URL = "jdbc:derby:CatalogDB";
+        Statement stmt = null;
+        Connection conn = null;
+        try {
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL);
+            System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT ProdNum, Description, Price FROM Catalog";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                String id = rs.getString("ProdNum");
+                double price = rs.getDouble("Price");
+                String description = rs.getString("Description");
+
+                out.print("ID: " + id.trim());
+                out.print(", Price: " + price);
+                out.println(", Description: " + description);
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null) stmt.close();
+            } catch (SQLException se2) {
+            }
+            try {
+                if (conn != null) conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+
+        /*out.println("<html><body><table><tr><th>Product ID</th><th>Product Name</th><th>Price</th></tr>");
+
+
+        InventoryService inventory = new InventoryService();
+        for (Product p : inventory.getAllProducts()
+        ) {
+            out.println("<tr><td>" + p.getId() + "</td><td>" + p.getName() + "</td><td>" + p.getPrice() + "</td></tr>");
+            out.print("href=\"DeleteController?product=" + URLEncoder.encode(p.getName(),"UTF-8") + "\">Remove");
+        }
+
+        out.println("</body></html>");*/
     }
 }
--- week5-2/src/us/plee19/models/InventoryService.java
+++ week5-2/src/us/plee19/models/InventoryService.java
@@ -1,12 +1,87 @@
 package us.plee19.models;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 import static java.util.Arrays.asList;
 
 public class InventoryService {
 
-    private List<Product> productList = asList(
+    public InventoryService() {
+        try {
+            final String DB_URL = "jdbc:derby:CatalogDB;create=true";
+            Connection conn = DriverManager.getConnection(DB_URL);
+            dropTables(conn);
+            buildCatalogTable(conn);
+            conn.close(); }
+        catch (Exception e) {
+            System.out.println("Error Creating the Catalog Table");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void dropTables(Connection conn) {
+        System.out.println("Checking for existing tables.");
+        try {
+            Statement stmt = conn.createStatement();
+            try {
+                stmt.execute("DROP TABLE Catalog");
+                System.out.println("Catalog table dropped.");
+            } catch (SQLException ex) {}
+        } catch (SQLException ex) {
+            System.out.println("ERROR: " + ex.getMessage());
+        }
+    }
+
+    public static void buildCatalogTable(Connection conn) {
+        try {
+            Statement stmt = conn.createStatement();
+            stmt.execute("CREATE TABLE Catalog (" +
+                    "Description CHAR(25)," +
+                    "ProdNum CHAR(10) NOT NULL PRIMARY KEY, " +
+                    "Price DOUBLE " +
+                    ")");
+
+            stmt.execute("INSERT INTO Catalog VALUES ( " +
+                    "'Pizza', " +
+                    "'1', " +
+                    "12.99 )");
+
+            stmt.execute("INSERT INTO Catalog VALUES ( " +
+                    "'Pasta', " +
+                    "'2', " +
+                    "11.99 )");
+
+            stmt.execute("INSERT INTO Catalog VALUES ( " +
+                    "'Tiramisu', " +
+                    "'3', " +
+                    "8.99 )");
+
+            stmt.execute("INSERT INTO Catalog VALUES ( " +
+                    "'Coffee', " +
+                    "'4', " +
+                    "1.99 )");
+
+            stmt.execute("INSERT INTO Catalog VALUES ( " +
+                    "'Soda', " +
+                    "'5', " +
+                    "2.99 )");
+
+            stmt.execute("INSERT INTO Catalog VALUES ( " +
+                    "'Cookies', " +
+                    "'6', " +
+                    "1.29 )");
+
+            System.out.println("Catalog table created.");
+        } catch (SQLException ex) {
+            System.out.println("ERROR: " + ex.getMessage());
+        }
+    }
+
+    /*private List<Product> productList = asList(
             new Product(1, "Pizza", 12.99),
             new Product(2, "Pasta", 11.99),
             new Product(3, "Tiramisu", 8.99),
@@ -26,5 +101,5 @@ public class InventoryService {
             }
         }
         return product;
-    }
+    }*/
 }
